593ee831a066f96d228c45fb02c04255dbe698f7
daffa i <daffa2.ismail178@gmail.com>
2026-02-24 06:50:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://${process.env.DOMAIN}:${process.env.PORT}`);
+});
+
