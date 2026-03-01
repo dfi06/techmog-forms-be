3cf8657aa125acfc4e1e783bc7b3970ec370d626
daffa i <daffa2.ismail178@gmail.com>
2026-03-01 22:03:27 +0700

    cleanup

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const allowedOrigins = [
-  "https://techmog-forms-fe.vercel.app",
-  "http://localhost:3000",
-  FRONTEND_URL,
-].filter(Boolean);
+const allowedOrigins = ["https://techmog-forms-fe.vercel.app"];
 
 app.use(
   cors({
@@ -37,8 +33,6 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 start = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
-  console.log(mongoose.connection.name);
-  console.log(FRONTEND_URL);
 
   app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
